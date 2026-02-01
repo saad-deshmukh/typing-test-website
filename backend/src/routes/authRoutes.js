38c0fadb681f5e28ff8dd11e667b9d084afb4d1b
saad-deshmukh <deshmukhsaad13@gmail.com>
2026-02-01 12:10:06 +0530

    CommonJS to ESM migration

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,17 @@
-const express = require("express");
+import express from "express";
+import { signup, login, logout, getMe } from "../controllers/authController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
 const router = express.Router();
-const { signup, login } = require("../controllers/authController");
 
 router.post("/signup", signup);
 router.post("/login", login);
 
-module.exports = router;
+//Logout route (clears cookie)
+router.post("/logout", authMiddleware, logout);
+
+// Persist login on refresh (checks cookie)
+// used authMiddleware here to verify the cookie before sending user data
+router.get("/me", authMiddleware, getMe);
+
+export default router;
