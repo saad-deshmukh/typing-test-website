fdd03296da411de13f32a458661047ba72a93bd8
saad-deshmukh <deshmukhsaad13@gmail.com>
2026-02-02 11:00:40 +0530

    Update db.js to use production MySQL configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,15 +4,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false
-  }
-);
+const sequelize = process.env.MYSQL_URL
+  ? new Sequelize(process.env.MYSQL_URL, {
+      dialect: "mysql",
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false,
+      }
+    );
 
 export default sequelize;
